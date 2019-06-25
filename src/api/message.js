4ea72301e6e09358c274f302de9b5206f0d0ea07
veraLX <734220574@qq.com>
2019-06-26 00:18:26 +0800

    update message

--- src/api/message.js
+++ src/api/message.js
@@ -1,8 +1,15 @@
 import axios from '@/libs/api.request'
 
-export const getMessageList = (data) => {
+export const getMessageList = (data) => { // 0 景点    1 活动
   return axios.request({
     url: 'admin/discuss/list?pageindex=' + data.page + '&pagesize=' + data.pagesize + '&distype=' + data.distype,
+    method: 'get'
+  })
+}
+
+export const getSpecialMessageList = (data) => { // 1 1 活动景点
+  return axios.request({
+    url: 'admin/discuss/list?pageindex=' + data.page + '&pagesize=' + data.pagesize + '&distype=' + data.distype + '&scenerytype' + data.scenerytype,
     method: 'get'
   })
 }
--- src/view/message-management/index.vue
+++ src/view/message-management/index.vue
@@ -5,24 +5,8 @@
         <p style="display:inline">留言</p>&nbsp;&nbsp;
         <Tag color="red">待审批留言数量 {{unresoluved}} 条</Tag>
       </div>
-      <Tabs value="sight" @on-click="tabsChange">
-        <!-- <TabPane label="首页留言" name="app">
-          <Table
-            :columns="appMessageColumns"
-            :data="appMessageList"
-            @on-selection-change="onAppSelectionChange"
-          ></Table>
-          <Page :total="appCount" @on-change="appChange"/>
-          <Row>
-            <Col span="2">
-              <Button @click="acceptance" type="primary">一键受理</Button>
-            </Col>
-            <Col span="2" style="margin-left:10px">
-              <Button @click="refuse" type="error">一键拒绝</Button>
-            </Col>
-          </Row>
-        </TabPane> -->
-        <TabPane label="景点留言" name="sight">
+      <Tabs value="commonSight" @on-click="tabsChange">
+        <TabPane label="普通景点留言" name="commonSight">
           <Table
             stripe
             :columns="sightMessageColumns"
@@ -36,6 +20,22 @@
             </Col>
             <Col span="2" style="margin-left:10px">
               <Button @click="sightDeleteFun" type="error">一键删除</Button>
+            </Col>
+          </Row>
+        </TabPane>
+        <TabPane label="活动景点留言" name="activitySight">
+          <Table
+            :columns="activitySightMessageColumns"
+            :data="sightActivityMessageList"
+            @on-selection-change="onSightActivitySelectionChange"
+          ></Table>
+          <Page :total="sightActivityCount" @on-change="sightActivityChange"/>
+          <Row>
+            <Col span="2">
+              <Button @click="sightActivityPass" type="primary">一键通过</Button>
+            </Col>
+            <Col span="2" style="margin-left:10px">
+              <Button @click="sightActivityDeleteFun" type="error">一键删除</Button>
             </Col>
           </Row>
         </TabPane>
@@ -67,126 +67,6 @@ export default {
   name: 'directive_page',
   data () {
     return {
-      appMessageColumns: [
-        { type: 'selection', width: 60, align: 'center', key: 'isCkeck' },
-        // { title: ' ', type: 'index', width: 60, align: 'center' },
-        { title: '学生姓名', key: 'studentName' },
-        { title: '学籍号', key: 'studentid' },
-        { title: '留言日期',
-          key: 'createdate',
-          render: (h, params) => {
-            let index = params.row.createdate.indexOf('T')
-            let time = params.row.createdate.slice(0, index)
-            return h(
-              'div',
-              time
-            )
-          }
-        },
-        { title: '留言内容', key: 'content', width: 300 },
-        {
-          title: '状态',
-          key: 'shstate',
-          width: 100,
-          render: (h, params) => {
-            return h(
-              'div',
-              params.row.shstate
-                ? params.row.shstate === 3
-                  ? '已受理'
-                  : '已拒绝'
-                : ''
-            )
-          }
-        },
-        // {
-        //   title: '是否推荐',
-        //   key: 'isrecommend',
-        //   width: 140,
-        //   align: 'center',
-        //   render: (h, params) => {
-        //     return h('div', [
-        //       h('i-switch', {
-        //         props: {
-        //           value: params.row.isrecommend === 1,
-        //           size: 'small'
-        //         },
-        //         on: {
-        //           'on-change': async () => {
-        //             await recommendMessage({
-        //               id: params.row.discussID,
-        //               isrecommend: params.row.isrecommend === 0 ? 1 : 0
-        //             })
-        //             this.getMessageList()
-        //           }
-        //         }
-        //       })
-        //     ])
-        //   }
-        // },
-        {
-          title: '操作',
-          key: 'action',
-          width: 140,
-          align: 'center',
-          options: ['delete'],
-          render: (h, params) => {
-            return h('div', [
-              h(
-                'Button',
-                {
-                  style: { 'margin-right': '8px' },
-                  props: {
-                    type: 'primary',
-                    size: 'small'
-                  },
-                  on: {
-                    click: async () => {
-                      await acceptMessage({
-                        id: params.row.discussID,
-                        shstate: 3
-                      })
-                      this.getMessageList()
-                    }
-                  }
-                },
-                '受理'
-              ),
-              h(
-                'Poptip',
-                {
-                  props: {
-                    confirm: true,
-                    title: '你确定要拒绝吗?'
-                  },
-                  on: {
-                    'on-ok': async () => {
-                      await acceptMessage({
-                        id: params.row.discussID,
-                        shstate: 4
-                      })
-                      this.getMessageList()
-                    }
-                  }
-                },
-                [
-                  h(
-                    'Button',
-                    {
-                      props: {
-                        type: 'error',
-                        size: 'small',
-                        disabled: params.row.isOriginal
-                      }
-                    },
-                    '拒绝'
-                  )
-                ]
-              )
-            ])
-          }
-        }
-      ],
       sightMessageColumns: [
         { type: 'selection', width: 60, align: 'center' },
         // { title: ' ', type: 'index', width: 60, align: 'center' },
@@ -219,31 +99,101 @@ export default {
             )
           }
         },
-        // {
-        //   title: '是否推荐',
-        //   key: 'isrecommend',
-        //   width: 140,
-        //   align: 'center',
-        //   render: (h, params) => {
-        //     return h('div', [
-        //       h('i-switch', {
-        //         props: {
-        //           value: params.row.isrecommend === 1,
-        //           size: 'small'
-        //         },
-        //         on: {
-        //           'on-change': async () => {
-        //             await recommendMessage({
-        //               id: params.row.discussID,
-        //               isrecommend: params.row.isrecommend === 0 ? 1 : 0
-        //             })
-        //             this.getMessageList()
-        //           }
-        //         }
-        //       })
-        //     ])
-        //   }
-        // },
+        {
+          title: '操作',
+          key: 'action',
+          width: 140,
+          align: 'center',
+          options: ['delete'],
+          render: (h, params) => {
+            return h('div', [
+              h(
+                'Button',
+                {
+                  style: { 'margin-right': '8px' },
+                  props: {
+                    type: 'primary',
+                    size: 'small'
+                  },
+                  on: {
+                    click: async () => {
+                      await acceptMessage({
+                        id: params.row.discussID,
+                        shstate: 1
+                      })
+                      this.getMessageList()
+                    }
+                  }
+                },
+                '通过'
+              ),
+              h(
+                'Poptip',
+                {
+                  props: {
+                    confirm: true,
+                    title: '你确定要删除吗?'
+                  },
+                  on: {
+                    'on-ok': async () => {
+                      await acceptMessage({
+                        id: params.row.discussID,
+                        shstate: 2
+                      })
+                      this.getMessageList()
+                    }
+                  }
+                },
+                [
+                  h(
+                    'Button',
+                    {
+                      props: {
+                        type: 'error',
+                        size: 'small',
+                        disabled: params.row.isOriginal
+                      }
+                    },
+                    '删除'
+                  )
+                ]
+              )
+            ])
+          }
+        }
+      ],
+      activitySightMessageColumns: [
+        { type: 'selection', width: 60, align: 'center' },
+        // { title: ' ', type: 'index', width: 60, align: 'center' },
+        { title: '学生姓名', key: 'studentName' },
+        { title: '学籍号', key: 'studentid' },
+        { title: '留言日期',
+          key: 'createdate',
+          render: (h, params) => {
+            let index = params.row.createdate.indexOf('T')
+            let time = params.row.createdate.slice(0, index)
+            return h(
+              'div',
+              time
+            )
+          }
+        },
+        { title: '留言内容', key: 'content', width: 300 },
+        {
+          title: '状态',
+          key: 'shstate',
+          width: 100,
+          render: (h, params) => {
+            return h(
+              'div',
+              params.row.shstate
+                ? params.row.shstate === 1
+                  ? '已通过'
+                  : '已删除'
+                : ''
+            )
+          }
+        },
         {
           title: '操作',
           key: 'action',
@@ -339,31 +289,6 @@ export default {
             )
           }
         },
-        // {
-        //   title: '是否推荐',
-        //   key: 'isrecommend',
-        //   width: 140,
-        //   align: 'center',
-        //   render: (h, params) => {
-        //     return h('div', [
-        //       h('i-switch', {
-        //         props: {
-        //           value: params.row.isrecommend === 1,
-        //           size: 'small'
-        //         },
-        //         on: {
-        //           'on-change': async () => {
-        //             await recommendMessage({
-        //               id: params.row.discussID,
-        //               isrecommend: params.row.isrecommend === 0 ? 1 : 0
-        //             })
-        //             this.getMessageList()
-        //           }
-        //         }
-        //       })
-        //     ])
-        //   }
-        // },
         {
           title: '操作',
           key: 'action',
@@ -427,13 +352,13 @@ export default {
           }
         }
       ],
-      appMessageList: [],
+      sightActivityMessageList: [],
       sightMessageList: [],
       activityMessageList: [],
       sightCount: 0,
-      appCount: 0,
+      sightActivityCount: 0,
       activityCount: 0,
-      appMessageSelectedList: [],
+      sightActivityMessageSelectedList: [],
       sightMessageSelectedList: [],
       activityMessageSelectedList: [],
       unresoluved: 0
@@ -445,36 +370,45 @@ export default {
   methods: {
     async getMessageList () {
       this.unresoluved = 0
+      // 景点列表
       const sightList = await getMessageList({
         page: 1,
         pagesize: 10,
         distype: 0
       })
-      this.sightMessageList = sightList.data.data.data
-        ? sightList.data.data.data
-        : []
+      let sortList1 = []
+      let sortNumber1 = 0
       _.each(sightList.data.data.data, (list) => {
         if (!list.shstate) {
           this.unresoluved++
         }
+        if (list.scenerytype !== 1) {
+          sortList1.push(list)
+          sortNumber1++
+        }
       })
-      this.sightCount = sightList.data.data.count
-        ? sightList.data.data.count
-        : 0
-      const appList = await getMessageList({
+      this.sightMessageList = sortList1 || []
+      this.sightCount = sortNumber1 || 0
+      // 活动景点列表
+      const sightActivityList = await getMessageList({
         page: 1,
         pagesize: 10,
-        distype: 3
+        distype: 0
       })
-      this.appMessageList = appList.data.data.data
-        ? appList.data.data.data
-        : []
-      // _.each(appList.data.data.data, (list) => {
-      //   if (!list.shstate) {
-      //     this.unresoluved++
-      //   }
-      // })
-      this.appCount = appList.data.data.count ? appList.data.data.count : 0
+      let sortList = []
+      let sortNumber = 0
+      _.each(sightActivityList.data.data.data, (list) => {
+        if (!list.shstate) {
+          this.unresoluved++
+        }
+        if (list.scenerytype === 1) {
+          sortList.push(list)
+          sortNumber++
+        }
+      })
+      this.sightActivityMessageList = sortList || []
+      this.sightActivityCount = sortNumber || 0
+      // 活动列表
       const activityList = await getMessageList({
         page: 1,
         pagesize: 10,
@@ -488,32 +422,34 @@ export default {
           this.unresoluved++
         }
       })
-      this.activityCount = activityList.data.data.count
-        ? activityList.data.data.count
-        : 0
+      this.activityCount = activityList.data.data.counta ? activityList.data.data.counta : 0
     },
     getDistype (id) {
       // distype：留言类型0,景点; 1,活动,2 学校,3首页
-      switch (id) {
-        case 0:
-          return '景点'
-        case 1:
-          return '活动'
-        case 2:
-          return '学校'
-        case 3:
-          return '首页'
-      }
+      // switch (id) {
+      //   case 0:
+      //     return '景点'
+      //   case 1:
+      //     return '活动'
+      //   case 2:
+      //     return '学校'
+      //   case 3:
+      //     return '首页'
+      // }
     },
-    async appChange (e) {
-      const appList = await getMessageList({
+    async sightActivityChange (e) {
+      const sightActivityList = await getMessageList({
         page: e,
         pagesize: 10,
-        distype: 3
+        distype: 0
       })
-      this.appMessageList = appList.data.data.data
-        ? appList.data.data.data
-        : []
+      let sortList = []
+      _.each(sightActivityList.data.data.data, (list) => {
+        if (list.scenerytype === 1) {
+          sortList.push(list)
+        }
+      })
+      this.sightActivityMessageList = sortList || []
     },
     async sightChange (e) {
       const sightList = await getMessageList({
@@ -521,9 +457,13 @@ export default {
         pagesize: 10,
         distype: 0
       })
-      this.sightMessageList = sightList.data.data.data
-        ? sightList.data.data.data
-        : []
+      let sortList = []
+      _.each(sightList.data.data.data, (list) => {
+        if (list.scenerytype === 1) {
+          sortList.push(list)
+        }
+      })
+      this.sightMessageList = sortList || []
     },
     async activityChange (e) {
       const activityList = await getMessageList({
@@ -540,20 +480,20 @@ export default {
       this.sightMessageSelectedList = []
       this.activityMessageSelectedList = []
     },
-    async acceptance () {
-      if (this.appMessageSelectedList.length !== 0) {
-        for (const item of this.appMessageSelectedList) {
-          await acceptMessage({ id: item.discussID, shstate: 3 })
+    async sightActivityPass () {
+      if (this.sightActivityMessageSelectedList.length !== 0) {
+        for (const item of this.sightActivityMessageSelectedList) {
+          await acceptMessage({ id: item.discussID, shstate: 1 })
         }
         this.getMessageList()
       } else {
         this.$Message.info('请选择要通过的留言')
       }
     },
-    async refuse () {
-      if (this.appMessageSelectedList.length !== 0) {
-        for (const item of this.appMessageSelectedList) {
-          await acceptMessage({ id: item.discussID, shstate: 4 })
+    async sightActivityDeleteFun () {
+      if (this.sightActivityMessageSelectedList.length !== 0) {
+        for (const item of this.sightActivityMessageSelectedList) {
+          await acceptMessage({ id: item.discussID, shstate: 2 })
         }
         this.getMessageList()
       } else {
@@ -564,10 +504,6 @@ export default {
       if (this.sightMessageSelectedList.length !== 0) {
         for (const item of this.sightMessageSelectedList) {
           await acceptMessage({ id: item.discussID, shstate: 1 })
-          // await recommendMessage({
-          //   id: item.discussID,
-          //   isrecommend: 1
-          // })
         }
         this.getMessageList()
       } else {
@@ -578,10 +514,6 @@ export default {
       if (this.sightMessageSelectedList.length !== 0) {
         for (const item of this.sightMessageSelectedList) {
           await acceptMessage({ id: item.discussID, shstate: 2 })
-          // await recommendMessage({
-          //   id: item.discussID,
-          //   isrecommend: 0
-          // })
         }
         this.getMessageList()
       } else {
@@ -611,9 +543,9 @@ export default {
     onSelect (e) {
       // console.log('onSelect', e)
     },
-    onAppSelectionChange (list) {
-      this.appMessageSelectedList = list
-      // console.log('onAppSelectionChange', this.appMessageSelectedList)
+    onSightActivitySelectionChange (list) {
+      this.sightActivityMessageSelectedList = list
+      // console.log('onSightActivitySelectionChange', this.sightActivityMessageSelectedList)
     },
     onSightSelectionChange (list) {
       this.sightMessageSelectedList = list
